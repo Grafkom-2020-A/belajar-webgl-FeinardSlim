75ae4548e637fc55a0252c3387542e6fb6af9997
FeinardSlim <58687783+FeinardSlim@users.noreply.github.com>
2020-10-24 00:46:55 +0700

    Update untuk tugas

--- main.js
+++ main.js
@@ -2,22 +2,63 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
-    var vertexShaderCode = `
-    void main(){
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-        gl_PointSize = 10.0;
-    }
-    `;
+    // Definisi verteks-verteks pada segitiga
+    /**
+     * A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5); D (0.5, 0.5)
+     */
+    var vertices = [
+        //Huruf
+        -0.9, 0.9, 1.0, 0.0, 0.0, // Titik A 
+        -0.9, -0.9, 0.0, 1.0, 0.0, // Titik B
+        -0.5, 0.9, 0.0, 0.0, 1.0, // Titik C
+        -0.5, -0.9, 1.0, 1.0, 1.0, // Titik D
+
+        //titik 0
+        //outer border
+        0.2, 0.9, 1.0, 1.0, 1.0,
+        0.7, 0.9, 1.0, 0.0, 1.0,
+        0.7, 0.1, 1.0, 1.0, 0.0,
+        0.2, 0.1, 0.0, 1.0, 1.0,
+
+        //inner border
+        0.3, 0.8, 1.0, 0.0, 0.0,
+        0.6, 0.8, 1.0, 0.0, 1.0,
+        0.6, 0.2, 1.0, 1.0, 0.0,
+        0.3, 0.2, 1.0, 1.0, 1.0,
+
+        //titik 8
+        //outer border
+        0.2, -0.9, 0.0, 0.0, 1.0,
+        0.2, -0.0, 0.0, 1.0, 0.0,
+        0.7, -0.0, 0.0, 1.0, 1.0,
+        0.7, -0.9, 1.0, 0.0, 0.0,
+
+        // inner border 1
+        0.3, -0.1, 1.0, 0.5, 1.0,
+        0.3, -0.4, 1.0, 1.0, 0.5,
+        0.6, -0.4, 0.5, 1.0, 1.0,
+        0.6, -0.1, 1.0, 0.5, 0.5,
+
+        // inner border 2
+        0.3, -0.5, 0.5, 1.0, 0.5,
+        0.3, -0.8, 0.5, 0.5, 1.0,
+        0.6, -0.8, 1.0, 0.5, 0,
+        0.6, -0.5, 0.5, 1.0, 0
+
+    ];
+
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var vertexShaderCode = document.getElementById("vertexShaderCode").text;
 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
 
-    var fragmentShaderCode = `
-    void main(){
-        gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
-    }
-    `;
+    var fragmentShaderCode = document.getElementById("fragmentShaderCode").text;
 
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
@@ -25,12 +66,45 @@ function main() {
 
     var shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader)
+    gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    gl.clearColor(0.2, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+    var aColor = gl.getAttribLocation(shaderProgram, "a_Color");
+    gl.vertexAttribPointer(
+        aPosition,
+        2,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        0);
+    gl.vertexAttribPointer(
+        aColor,
+        3,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aPosition);
+    gl.enableVertexAttribArray(aColor);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.clearColor(0.0, 0.0, 0.0, 0.8);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.viewport(100, 0, canvas.height, canvas.height);
+
+    var primitive = gl.TRIANGLE_STRIP;
+    var offset = 0; // Titik Penggambaran
+    var count = 4; // Jumlah verteks yang akan digambar
+    gl.drawArrays(primitive, offset, count);
+
+    var i = 0;
+    while (i != 5) {
+        i++;
+        primitive = gl.LINE_LOOP;
+        offset += 4; // Titik Penggambaran
+        count = 4; // Jumlah verteks yang akan digambar
+        gl.drawArrays(primitive, offset, count);
+    }
 }
